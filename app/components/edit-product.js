cd3ab9c567322f600c1d02dde31757b6be031303
EPHIEPROGRAMMER <ephantus.muthunga@gmail.com>
2017-04-07 14:17:05 +0300

    editing-product- created edit-product component.where the admin can update on the existing products

--- app/components/edit-product.js
+++ app/components/edit-product.js
@@ -1,4 +1,24 @@
+//this Component allows the admin to update Product in this case(maclub)
+
+
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  updateProduct: false,
+  actions: {
+    updateProductFormShow() {
+      this.set('updateProduct', true);
+    },
+    update(product) {
+      var params = {
+        title: this.get("title"),
+        description: this.get("description"),
+        venue: this.get("venue"),
+        image: this.get("image")
+      };
+      this.set('updateProduct', false);
+      this.sendAction('update', product, params);
+      // debugger;
+    }
+  }
 });
--- app/components/new-product.js
+++ app/components/new-product.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    save() {
+      var params = {
+      title: this.get('title'),
+      description: this.get('description'),
+      value: this.get('value'),
+      image: this.get('image')
+    };
+    this.sendAction('save', params);
+    }
+  }
 });

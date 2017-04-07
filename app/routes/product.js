121f0aa58a2a3198414cd3eb10010ed21180f8c1
EPHIEPROGRAMMER <ephantus.muthunga@gmail.com>
2017-04-07 15:12:35 +0300

    feedback- feedback-til and feedback-product where users can feedback on a club

--- app/routes/product.js
+++ app/routes/product.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(params) {
+    return this.store.findRecord('product', params.product_id);
+  },
+  
+    saveFeedback(params) {
+      var newFeedback = this.store.createRecord('feedback', params);
+      var product = params.product;
+      product.get('feedbacks').addObject(newFeedback);
+      newFeedback.save().then(function() {
+        return product.save();
+      });
+      this.transitionTo('product', params.product_id);
+    }
+  }
 });

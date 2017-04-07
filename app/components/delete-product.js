a50cd586247d3ef8da2d91985dd92152c37ba6d9
EPHIEPROGRAMMER <ephantus.muthunga@gmail.com>
2017-04-07 14:23:46 +0300

    Deleting product created a delete-product component where admin can delete on the selected club

--- app/components/delete-product.js
+++ app/components/delete-product.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    delete(product) {
+      if (confirm('Are you sure you want to delete this product?... No but seriously, you sure?')) {
+        this.sendAction('destroyProduct', product);
+      }
+    }
+  }
 });
+//promps the admin whether he is sure if he want to delete the product(club)
